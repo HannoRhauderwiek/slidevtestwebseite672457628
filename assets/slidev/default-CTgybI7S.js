543ff8ba7c12963c0f6f660d914972a65429b6c0
Hanno <h.rhauderwiek@icloud.com>
2025-12-09 23:15:23 +0100

    biu

--- assets/slidev/default-CTgybI7S.js
+++
@@ -1,1 +0,0 @@
-import{f as o,o as t,j as s}from"../modules/vue-DTs8Rfa_.js";import{_ as r}from"../index-BpJtr8fU.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
